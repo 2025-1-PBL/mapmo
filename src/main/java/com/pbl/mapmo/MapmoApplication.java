838e3247e5065f783e9878aa29143859094b2930
ocb3916 <dhchanbin1004@gmail.com>
2025-03-31 18:16:24 +0900

    스프링 부트 개발 환경 - JDK21버전

---
+++ src/main/java/com/pbl/mapmo/MapmoApplication.java
@@ -0,0 +1,13 @@
+package com.pbl.mapmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapmoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pbl/mapmo/MapmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbl.mapmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
